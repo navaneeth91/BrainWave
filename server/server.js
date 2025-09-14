bb9abb1cefdd074ae74654916d973b958e539d73
Navaneeth91 <navaneethsiliveri7288@gmail.com>
2025-09-14 11:29:06 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(clerkMiddleware())
 
 // Routes
 app.get('/',(req, res)=>res.send("API Working"))
-app.post("/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhooks);
+app.post('/clerk' ,express.json(),clerkWebhooks)
 app.use('/api/educator',express.json(),eductaorRouter)
 app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter);
