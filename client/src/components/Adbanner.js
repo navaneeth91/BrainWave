f9492eb5be80dba0d21e99b7a7ce7bc6c525a043
Navaneeth Siliveri <154795403+navaneeth91@users.noreply.github.com>
2026-01-03 00:11:40 +0530

    Add Adbanner component for Google Ads integration

---
+++ client/src/components/Adbanner.js
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5495260329408395"
+     crossorigin="anonymous"></script>
+<!-- footeradd -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5495260329408395"
+     data-ad-slot="2038724918"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
