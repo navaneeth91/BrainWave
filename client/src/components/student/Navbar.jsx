867cd30f3b7b9496e04b47501d0fc3f01a50bfb5
Navaneeth Siliveri <154795403+navaneeth91@users.noreply.github.com>
2026-07-21 23:50:08 +0530

    Update Navbar.jsx

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
         ) : (
           <button
             onClick={() => openSignIn()}
-            className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
           >
             Create Account
           </button>
