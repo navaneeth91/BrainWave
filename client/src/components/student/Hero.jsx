40cab886e3048103fca3b14f62c327a3227c0c07
Navaneeth Siliveri <154795403+navaneeth91@users.noreply.github.com>
2026-07-21 23:53:10 +0530

    Update Hero.jsx

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-blue-200/70 '>
+    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-blue-300/70 '>
       <h1 className='md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto '>Unlock Your Potential with the courses by <span className='text-orange-600'>BrainWave</span>
       </h1>
       <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'> We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.</p>
